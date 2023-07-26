8889259066de64414cf421a9969cbaa5c4b3e31e
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-07-26 18:08:50 +0100

    Update show.blade.php

--- resources/views/admin/admins/show.blade.php
+++ resources/views/admin/admins/show.blade.php
@@ -249,28 +249,13 @@
                                 <img src="{{ asset('storage/images/proof/'.$vendorDetails['vendor_business']['means_id_proof']) }}" alt="" style="width:100%;">
                             </a>
                             @endif
+                            @if (!empty($vendorDetails['vendor_business']['license_proof']))
+                            <a href="{{ asset('storage/images/proof/'.$vendorDetails['vendor_business']['license_proof']) }}" data-exthumbimage="{{ asset('storage/images/proof/'.$vendorDetails['vendor_business']['license_proof']) }}" data-src="images/big/img1.jpg" class="col-lg-3 col-md-6 mb-4">
+                                <img src="{{ asset('storage/images/proof/'.$vendorDetails['vendor_business']['license_proof']) }}" alt="" style="width:100%;">
+                            </a>
+                            @endif
 
-                            <a href="{{ asset('admin/images/big/img2.jpg') }}" data-exthumbimage="{{ asset('admin/images/big/img2.jpg') }}" data-src="{{ asset('admin/images/big/img2.jpg') }}" class="col-lg-3 col-md-6 mb-4">
-                                <img src="{{ asset('admin/images/big/img2.jpg') }}" alt="" style="width:100%;">
-                            </a>
-                            <a href="images/big/img3.jpg" data-exthumbimage="images/big/img3.jpg" data-src="images/big/img3.jpg" class="col-lg-3 col-md-6 mb-4">
-                                <img src="images/big/img3.jpg" alt="" style="width:100%;">
-                            </a>
-                            <a href="images/big/img4.jpg" data-exthumbimage="images/big/img4.jpg" data-src="images/big/img4.jpg" class="col-lg-3 col-md-6 mb-4">
-                                <img src="images/big/img4.jpg" alt="" style="width:100%;">
-                            </a>
-                            <a href="images/big/img5.jpg" data-exthumbimage="images/big/img5.jpg" data-src="images/big/img5.jpg" class="col-lg-3 col-md-6 mb-4">
-                                <img src="images/big/img5.jpg" alt="" style="width:100%;">
-                            </a>
-                            <a href="images/big/img6.jpg" data-exthumbimage="images/big/img6.jpg" data-src="images/big/img6.jpg" class="col-lg-3 col-md-6 mb-4">
-                                <img src="images/big/img6.jpg" alt="" style="width:100%;">
-                            </a>
-                            <a href="images/big/img7.jpg" data-exthumbimage="images/big/img7.jpg" data-src="images/big/img7.jpg" class="col-lg-3 col-md-6 mb-4">
-                                <img src="images/big/img7.jpg" alt="" style="width:100%;">
-                            </a>
-                            <a href="images/big/img8.jpg" data-exthumbimage="images/big/img8.jpg" data-src="images/big/img8.jpg" class="col-lg-3 col-md-6 mb-4">
-                                <img src="images/big/img8.jpg" alt="" style="width:100%;">
-                            </a>
+
                         </div>
                     </div>
                 </div>

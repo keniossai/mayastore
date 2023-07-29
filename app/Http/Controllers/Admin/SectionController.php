4e0f679bccdf9c77599d45beb2e2bd4cbb78bbe9
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-07-29 20:08:48 +0100

    Create SectionController.php

---
+++ app/Http/Controllers/Admin/SectionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Section;
+use Illuminate\Http\Request;
+
+class SectionController extends Controller
+{
+    public function index(){
+        $sections = Section::get()->toArray();
+        dd($sections);
+        return view('admin.section.index');
+    }
+}

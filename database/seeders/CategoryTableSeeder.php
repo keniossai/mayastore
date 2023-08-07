70d3c446057789b2ad6255bfc8157ca927f881d7
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-08-07 20:19:32 +0100

    Create CategoryTableSeeder.php

---
+++ database/seeders/CategoryTableSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categoryRecords = [
+            [
+                'id' => 1,
+                'parent_id' => 0,
+                'section_id' => 1,
+                'category_name' => 'Men',
+                'category_image' => '',
+                'category_discount' => 0,
+                'description' => '',
+                'status' => 1,
+                'url' => 'men',
+                'meta_title' => '',
+                'meta_description' => '',
+                'meta_keywords' => '',
+
+            ],
+            [
+                'id' => 2,
+                'parent_id' => 0,
+                'section_id' => 1,
+                'category_name' => 'Women',
+                'category_image' => '',
+                'category_discount' => 0,
+                'description' => '',
+                'status' => 1,
+                'url' => 'women',
+                'meta_title' => '',
+                'meta_description' => '',
+                'meta_keywords' => '',
+
+            ],
+            [
+                'id' => 3,
+                'parent_id' => 0,
+                'section_id' => 1,
+                'category_name' => 'Kids',
+                'category_image' => '',
+                'category_discount' => 0,
+                'description' => '',
+                'status' => 1,
+                'url' => 'kids',
+                'meta_title' => '',
+                'meta_description' => '',
+                'meta_keywords' => '',
+
+            ]
+        ];
+        Category::insert($categoryRecords);
+    }
+}

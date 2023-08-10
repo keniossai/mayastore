f8c83f0c37392f7d0caea135fb7aac42550fe09b
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-08-10 21:11:07 +0100

    Update add-category.blade.php

--- resources/views/components/add-category.blade.php
+++ resources/views/components/add-category.blade.php
@@ -10,13 +10,13 @@
                 @csrf
                 <div class="row">
                     <div class="col-xl-12">
-                        <div class="mt-3">
+                        <div class="mt-1">
                             <label class="form-label">Category Name<span class="text-danger">*</span></label>
                             <input type="text" name="name" id="" class="form-control" placeholder="Enter Category name">
                         </div>
-                        <div class="mt-3">
+                        <div class="mt-1">
                             <label class="form-label">Select Section<span class="text-danger">*</span></label>
-                            <select class="form-control" name="" id="">
+                            <select class="form-control" class="" name="" id="">
                                 <?php $getSections = App\Models\Section::get()->toArray(); ?>
                                 <option value="">Select</option>
                                 @foreach ($getSections as $section)
@@ -24,23 +24,39 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="mt-3">
+                        <div class="mt-1">
                             <label class="form-label">Select Category Level<span class="text-danger">*</span></label>
                             <select name="parent_id" class="form-control" id="parent_id">
                                 <option value="0">Main Category</option>
                             </select>
                         </div>
-                        <div class="mt-3">
-                            <label class="form-label">Category Name<span class="text-danger">*</span></label>
-                            <input type="file" name="category_image" style="height: 40px;" id="" class="form-control" placeholder="Enter Category name">
+                        <div class="mt-1">
+                            <label class="form-label">Category Image<span class="text-danger">*</span></label>
+                            <input type="file" name="category_image" id="" class="form-control">
                         </div>
-                        <div class="mt-3">
+                        <div class="mt-1">
                             <label class="form-label">Category Discount<span class="text-danger">*</span></label>
-                            <input type="text" name="category_discount" id="" class="form-control" placeholder="Enter Category name">
+                            <input type="text" name="category_discount" id="" class="form-control" placeholder="Enter Category Discount">
                         </div>
-                        <div class="mt-3">
+                        <div class="mt-1">
                             <label class="form-label">Category Description<span class="text-danger">*</span></label>
-                            <textarea class="form-control" name="description" id="description"></textarea>
+                            <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+                        </div>
+                        <div class="mt-1">
+                            <label class="form-label">Category URL<span class="text-danger">*</span></label>
+                            <input type="text" name="url" id="url" class="form-control" placeholder="Enter URL">
+                        </div>
+                        <div class="mt-1">
+                            <label class="form-label">Meta Title<span class="text-danger">*</span></label>
+                            <input type="text" name="meta_title" id="meta_title" class="form-control" placeholder="Enter Meta Title">
+                        </div>
+                        <div class="mt-1">
+                            <label class="form-label">Meta Description<span class="text-danger">*</span></label>
+                            <input type="text" name="meta_description" id="meta_description" class="form-control" placeholder="Enter Meta Description">
+                        </div>
+                        <div class="mt-1">
+                            <label class="form-label">Meta Keywords<span class="text-danger">*</span></label>
+                            <input type="text" name="meta_keywords" id="meta_keywords" class="form-control" placeholder="Enter Meta Keywords">
                         </div>
                     </div>
                 </div>

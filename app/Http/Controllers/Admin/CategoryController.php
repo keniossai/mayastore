ef72ec74f51e4f1063ba5d17baf0a227596eb28d
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-08-19 22:45:15 +0100

    Update CategoryController.php

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use App\Models\Category;
 use App\Models\Section;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManagerStatic as Image;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
@@ -30,15 +31,85 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+    // public function storeEditCategory(Request $request, $id=null)
+    // {
+    //     if($id=""){
+    //         // Add category functionality
+    //         $title = "Add Category";
+    //         $category = New Category;
+    //         // $message('success','Category added successfully');
+    //     }else{
+    //         $title = "Edit Category";
+    //         $category = Category::find($id);
+    //         // $message('success','Category added successfully');
+    //     }
+
+    //     if($request->isMethod('post')){
+    //         $data = $request->all();
+    //         // dd($request->all());
+
+    //         if($request->hasFile('category_image')){
+    //             $image_tmp = $request->file('category_image');
+    //             if($image_tmp->isValid()){
+    //                 // Get Image Extension
+    //                 $extension = $image_tmp->getClientOriginalExtension();
+    //                 // Generate New Image Name
+    //                 $imageName = rand(111,99999).'.'.$extension;
+    //                 $imagePath = 'storage/images/category'.$imageName;
+    //                 // Upload Image
+    //                 Image::make($image_tmp)->save($imagePath);
+    //             }
+    //         }
+    //         $category = new Category;
+    //         $category->parent_id = $data['parent_id'];
+    //         $category->section_id = $data['section_id'];
+    //         $category->category_name = $data['category_name'];
+    //         $category->description = $data['description'];
+    //         $category->category_discount = $data['category_discount'];
+    //         $category->category_image = '';
+    //         $category->url = $data['url'];
+    //         $category->meta_title = $data['meta_title'];
+    //         $category->meta_description = $data['meta_description'];
+    //         $category->meta_keywords = $data['meta_keywords'];
+    //         $category->status = 1;
+    //         $category->save();
+
+
+    //         return redirect()->back()->with('success');
+    //     }
+
+    //     return view('admin.category.create', compact('category', 'title'));
+    // }
     public function store(Request $request)
     {
         if($request->isMethod('post')){
-            $data = $request->all();
+            $category = $request->all();
 
-            $data = new Category;
-            $data->category_name = $request->category_name;
-            $data->status = 1;
-            $data->save();
+            if($request->hasFile('category_image')){
+                $image_tmp = $request->file('category_image');
+                if($image_tmp->isValid()){
+                    // Get Image Extension
+                     $extension = $image_tmp->getClientOriginalExtension();
+                    // Generate New Image Name
+                     $imageName = rand(111,99999).'.'.$extension;
+                     $imagePath = 'storage/images/category'.$imageName;
+                    // Upload Image
+                     Image::make($image_tmp)->save($imagePath);
+                }
+            }
+            $category = new Category;
+            $category->category_name = $request->category_name;
+            $category->description = $request->description;
+            $category->parent_id = $request->parent_id;
+            $category->section_id = $request->section_id;
+            $category->category_discount = $request->category_discount;
+            $category->url = $request->url;
+            $category->meta_title = $request->meta_title;
+            $category->meta_description = $request->meta_description;
+            $category->meta_keywords = $request->meta_keywords;
+            $category->category_image = $request->category_image;
+            $category->status = 1;
+            $category->save();
 
 
             return redirect()->back()->with('success','Category added successfully');
@@ -70,9 +141,10 @@ class CategoryController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Request $request, $id)
     {
-        //
+        $category = Category::find($id);
+        return view('admin.category.edit', compact('category'));
     }
 
     /**

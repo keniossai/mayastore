89d13c1a08e41076dd7ea9dfe89f6f4d8462782d
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-08-29 19:37:03 +0100

    Update CategoryController.php

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -34,52 +34,34 @@ class CategoryController extends Controller
 
     // Add Category function
 
-    public function store(Request $request)
-    {
-        $category = new Category();
-        if($request->hasFile('category_image'))
-        {
-            $image_tmp = $request->file('category_image');
-            $extension = $image_tmp->getClientOriginalExtension();
-            $imageName = rand(111,99999).'.'.$extension;
-            $imagePath = 'storage/images/category'.$imageName;
-            Image::make($image_tmp)->save($imagePath);
-            $category->category_image = $imageName;
-
-        }
-
-            $category->category_name = $request->category_name;
-            $category->description = $request->description;
-            $category->parent_id = $request->parent_id;
-            $category->section_id = $request->section_id;
-            $category->category_discount = $request->category_discount;
-            $category->url = $request->url;
-            $category->meta_title = $request->meta_title;
-            $category->meta_description = $request->meta_description;
-            $category->meta_keywords = $request->meta_keywords;
-            $category->category_image = $imageName;
-            $category->status = 1;
-            $category->save();
 
 
+    public function store(Request $request){
 
-
-
-            return redirect()->back()->with('success','Category added successfully');
-
-            // if($request->hasFile('category_image')){
-            //     $image_tmp = $request->file('category_image');
-            //     if($image_tmp->isValid()){
-            //         // Get Image Extension
-            //          $extension = $image_tmp->getClientOriginalExtension();
-            //         // Generate New Image Name
-            //          $imageName = rand(111,99999).'.'.$extension;
-            //          $imagePath = 'storage/images/category'.$imageName;
-            //         // Upload Image
-            //          Image::make($image_tmp)->save($imagePath);
-            //     }
-            // }
-
+         #Handle File Upload
+         if($request->hasfile('banner_img'))
+            {
+                $name = rand(111,99999). '.' . $request->banner_img->getClientOriginalExtension();
+                $path = public_path() .'/storage/category';
+                $request->banner_img->move($path, $name);
+                $banner_img = $name;
+            }else{
+            $banner_img = null;
+            }
+        $cat = new Category();
+        $cat->category_name = $request->category_name;
+        $cat->section_id = $request->section_id;
+        $cat->parent_id = $request->parent_id;
+        $cat->cat_banner = $banner_img;
+        $cat->description = $request->description;
+        $cat->category_discount = $request->category_discount;
+        $cat->url = $request->url;
+        $cat->meta_title = $request->meta_title;
+        $cat->meta_description = $request->meta_description;
+        $cat->meta_keywords = $request->meta_keywords;
+        $cat->status = 1;
+        $cat->save();
+        return redirect()->back()->with('success','Category added successfully');
     }
 
     /**

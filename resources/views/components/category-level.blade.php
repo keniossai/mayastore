fb198b1f0a0b7f50fb8fac23d438f7868cbbbb83
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-08-31 20:58:18 +0100

    Update category-level.blade.php

--- resources/views/components/category-level.blade.php
+++ resources/views/components/category-level.blade.php
@@ -1,16 +1,13 @@
 <select value="" name="parent_id" class="form-control" id="parent_id">
-    <option value="0" >Main Category</option>
+    <option value="0" @if(isset($category['parent_id']) && $category['parent_id']==0) selected="" @endif>Main Category</option>
     @if (!empty($getCategories))
         @foreach ($getCategories as $category)
-            <option value="{{ $category['id'] }}" >{{ $category['category_name'] }}</option>
+            <option value="{{ $category['id'] }}" @if(isset($category['parent_id']) && $category['parent_id']==$category['id']) selected="" @endif>{{ $category['category_name'] }}</option>
+            @if (!empty($category['subcategories']))
+                @foreach ($category['subcategories'] as $subcategory)
+                    <option value="{{ $subcategory['id'] }}" @if(isset($category['parent_id']) && $category['parent_id']==$subcategory['id']) selected="" @endif>{{ $subcategory['category_name'] }}</option>
+                @endforeach
+            @endif
         @endforeach
     @endif
 </select>
-{{-- @if (!empty($category['subcategories']))
-    @foreach ($category['subcategories'] as $subcategory)
-        <option value="{{ $subcategory['id'] }}" @if(isset($category['parent_id']) && $category['parent_id']==$subcategory['id']) selected="" @endif>{{ $subcategory['category_name'] }}</option>
-    @endforeach
-@endif --}}
-{{-- @if(isset($category['parent_id']) && $category['parent_id']==$category['id']) selected="" @endif --}}
-
-{{-- @if(isset($category['parent_id']) && $category['parent_id']==0) selected="" @endif --}}
